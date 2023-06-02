4794b6d4e076c3708d8d8f254d300f7e257eaae5
jason <dozejason@gmail.com>
2023-06-02 17:10:10 -0500

    Add strict host checking to ansible.cfg in ansible_deploy.sh.
    Remove ansible.cfg in ansible_destroy.sh.
    
    Remove halt flag from shutdown now in shutdown_pi.yaml
    
    Signed-off-by: jason <dozejason@gmail.com>

--- ansible_deploy.sh
+++ ansible_deploy.sh
@@ -8,7 +8,7 @@ then
   echo -e "\n==== Homebrew installed ====\n"
 else 
   echo -e "\n==== Installing homebrew ====\n"
-  sudo true; NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install Ansible
@@ -20,6 +20,18 @@ else
   brew install ansible
 fi
 
+# Set strict host key checking to no.
+if [  -f ansible.cfg ]
+then
+  echo -e "\n==== Ansible.cfg present ====\n"
+else
+  echo -e "\n==== Creating ansible.cfg file ====\n"
+  cat <<- EOF > ansible.cfg
+[defaults]
+host_key_checking = False
+EOF
+fi
+
 # Dynamically add IP to inventory.ini file if Pi is running
 if ( ping -c 1 $PI_HOST )
 then
@@ -29,20 +41,17 @@ then
     echo -e "\n==== Inventory.ini file present ====\n"
   else
     echo -e "\n==== Create inventory.ini file  ====\n"
-  cat <<- EOF > inventory.ini
+    cat <<- EOF > inventory.ini
 [raspberrypi]
 $PI_HOST ansible_user=$USER
 EOF
   fi
+   # Run main playbook
+  if [ -f inventory.ini ]
+  then
+    echo -e "\n==== Running main playbook ====\n"
+    ansible-playbook -i inventory.ini main_playbook.yaml
+  fi
 else 
   echo -e "\n==== RaspberryPI server not running ====\n"
-fi
-
-# Run main playbook
-if [ -f inventory.ini ]
-then
-  echo -e "\n==== Running main playbook ====\n"
-  ansible-playbook -i inventory.ini main_playbook.yaml
-else
-  echo -e "\n==== Inventory file not present ====\n"
 fi
